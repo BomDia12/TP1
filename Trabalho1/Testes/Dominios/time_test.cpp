d01ef7174b9fb958806fd645704fcc5535f87171
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2022-02-23 14:28:47 -0300

    :white_check_mark: testes time

---
+++ Trabalho1/Testes/Dominios/time_test.cpp
@@ -0,0 +1,34 @@
+#include "time_test.hpp"
+#include <string>
+#include <iostream>
+using namespace std;
+
+void test_block(string time) {
+    try {
+        cout << "Horario testado: " << time << endl << endl;
+        Time obj = Time(time);
+        cout << "Valor aceito!" << endl;
+        cout << "O valor atual é: " << obj.getTime() << endl;
+    } catch(invalid_argument& message) {
+        cout << "Valor rejeitado!" << endl;
+        cout << "Mensagem de erro: " << message.what() << endl;
+    }
+    cout << "\n==============================\n\n";
+};
+
+int main () {
+
+    cout << "Testando com horario válido" << endl;
+
+    test_block("23:59");
+
+    cout << "Testando com data inválida" << endl;
+
+    test_block("24:00");
+
+    cout << "Testando com data inválida" << endl;
+
+    test_block("23:60");
+
+    return 0;
+};
