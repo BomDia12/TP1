6f510ca428c52318d468ab22beedc10cf6db951d
bomdia12 <chamatico2@gmail.com>
2022-02-25 11:37:11 -0300

    📝 documentation language

--- Trabalho1/Dominios/language.hpp
+++ Trabalho1/Dominios/language.hpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 /**
+ * @brief Classe do domínio Idioma
+ * 
  * @author Gabriel Cruz - 200049038
  */
 class Language {
@@ -15,9 +17,33 @@ class Language {
             "Indonesio"
         };
     public:
+
+        /**
+         * @brief Construtor padrão do domínio idioma
+         */
         Language();
+
+        /**
+         * @brief Construtor que iniciliza o objeto com o valor indicado
+         * 
+         * @param language valor a ser inicializado no objeto
+         * @throw invalid_argumnt caso o valor inserido não seja válido
+         */
         Language(string language);
+
+        /**
+         * @brief retorna o valor armazenado no objeto em questão
+         * 
+         * @return string com o idioma armazenado no objeto em questão
+         */
         string getLanguage();
+
+        /**
+         * @brief insere um valor no objeto em questão
+         * 
+         * @param language com a valor a ser inserido no objeto em questão
+         * @throw invalid_argument caso o valor inserido seja inválido
+         */
         void setLanguage(string language);
 };
 
