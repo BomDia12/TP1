fb7de5f612e41b356e10d7a3fd35dbeadc56427d
bomdia12 <chamatico2@gmail.com>
2022-02-25 11:50:24 -0300

    📝 password

--- Trabalho1/Dominios/password.hpp
+++ Trabalho1/Dominios/password.hpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 /**
+ * @brief Classe do Domínio senha
+ * 
  * @author Pedro Venzi -  200042696
  */
 class Password {
@@ -10,9 +12,33 @@ class Password {
         void validate(string value);
 
     public:
+
+        /**
+         * @brief Cosntrutor padrão do Domínio senha
+         */
         Password();
+
+        /**
+         * @brief Construtor que inicializa o objeto com o valor indicado
+         * 
+         * @param value valor a inicializar o objet em questão
+         * @throw invalid_argument caso o valor inserido não seja válido
+         */
         Password(string value);
+
+        /**
+         * @brief retorna a senha armazenada no objeto
+         * 
+         * @return string com a senha armazenada nesse objeto
+         */
         string getPassword();
+
+        /**
+         * @brief atualiza a senha desse objeto
+         * 
+         * @param value valor da nova senha
+         * @throw invalid_argument caso o valor inserido seja inválido
+         */
         void setPassword(string value);
 };
 
