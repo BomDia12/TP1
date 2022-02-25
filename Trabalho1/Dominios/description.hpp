0fab0ef8102f4c2c8b82f2b356e0ee0f65d3b2bb
bomdia12 <chamatico2@gmail.com>
2022-02-25 11:19:34 -0300

    📝 documentation description

--- Trabalho1/Dominios/description.hpp
+++ Trabalho1/Dominios/description.hpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 /**
+ * @brief Classe para o domínio descrição
+ * 
  * @author Pedro Venzi -  200042696
  */
 class Description {
@@ -12,9 +14,33 @@ class Description {
         void validate(string value);
 
     public:
+
+        /**
+         * @brief Construtor padrão
+         */
         Description();
+
+        /**
+         * @brief Construtor que inicializa o objeto com o valor fornecido
+         * 
+         * @param value valor a ser usado para inicializar a descrição
+         * @throw invalid_argument quando o valor inserido não é válido
+         */
         Description(string value);
+
+        /**
+         * @brief retorna o valor armazenado no objeto em questão
+         * 
+         * @return string com o valor da descrição
+         */
         string getDescription();
+
+        /**
+         * @brief insere um novo valor como descrição do objeto
+         * 
+         * @param value valor a ser inserido no objeto
+         * @throw invalid_argument caso o valor inserido não seja válido
+         */
         void setDescription(string value);
 };
 
