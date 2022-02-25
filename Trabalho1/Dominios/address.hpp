f9f97b752c416021095da8f2044965b585db27bd
bomdia12 <chamatico2@gmail.com>
2022-02-25 10:57:49 -0300

    📝 documentation Address

--- Trabalho1/Dominios/address.hpp
+++ Trabalho1/Dominios/address.hpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 /**
+ * @brief Classe para o domínio Address
+ * 
  * @author Pedro Venzi -  200042696
  */
 class Address {
@@ -10,9 +12,32 @@ class Address {
         void validate(string value);
 
     public:
+        /**
+         * @brief Construtor padrão para o Address
+         */
         Address();
+
+        /**
+         * @brief Construtor para o Addres que iniciliza com um valor, caso esse seja válido
+         * 
+         * @param value valor para inicializar o address, caso válido
+         * @throw invalid_agument caso o valor não seja válido
+         */
         Address(string value);
+
+        /**
+         * @brief retorna o endereço guardado no objeto em questão
+         * 
+         * @return string com o endereço guardado
+         */
         string getAddress();
+
+        /**
+         * @brief Configura o endereço do objeto em questão
+         * 
+         * @param value valor para inicializar o address, caso válido
+         * @throw invalid_agument caso o valor não seja válido 
+         */
         void setAddress(string value);
 };
 
