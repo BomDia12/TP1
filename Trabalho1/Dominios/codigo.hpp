291a99b9c2b353b75a6ab3aa8d40f7bcec83d04d
bomdia12 <chamatico2@gmail.com>
2022-02-25 11:07:40 -0300

    📝 documentaçao codigo

--- Trabalho1/Dominios/codigo.hpp
+++ Trabalho1/Dominios/codigo.hpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 /**
+ * @brief classe para o domínio descrição
+ * 
  * @author Arthur Mota Furtado - 200014935
  */
 class Codigo {
@@ -11,9 +13,33 @@ class Codigo {
         string codigo;
         void validate(string codigo);
     public:
+
+        /**
+         * @brief Construtor padrão
+         */
         Codigo();
+
+        /**
+         * @brief Construtor com um valor para inicializar o código
+         * 
+         * @param codigo valor a ser usado para inicializar o código
+         * @throw invalid_argument caso o valor seja inválido
+         */
         Codigo(string codigo);
+
+        /**
+         * @brief retorna o valor atual do objeto em questão
+         * 
+         * @return string com o valor do código
+         */
         string getCodigo();
+
+        /**
+         * @brief Configura um novo valor para o objeto em questão
+         * 
+         * @param codigo é o novo valor a ser inserido no objeto
+         * @throw invalid_argument caso o valor seja inválido
+         */
         void setCodigo(string codigo);
 };
 
