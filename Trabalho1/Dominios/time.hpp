0ee4a3050eae7eaa74aaa81d815becd923a5284a
bomdia12 <chamatico2@gmail.com>
2022-02-25 11:58:11 -0300

    📝 timew

--- Trabalho1/Dominios/time.hpp
+++ Trabalho1/Dominios/time.hpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 /**
+ * @brief Classe do domínio tempo
+ * 
  * @author Gabriel Cruz - 200049038
  */
 class Time {
@@ -10,9 +12,33 @@ class Time {
         string time;
         void validate(string time);
     public:
+
+        /**
+         * @brief Construtor padrão do domínio tempo
+         */
         Time();
+
+        /**
+         * @brief Construtor que inicializa o objeto com o valor inserido
+         * 
+         * @param time valor a inicializar o objeto
+         * @throw invalid_argumetn caso o valor inserido não seja válido
+         */
         Time(string time);
+
+        /**
+         * @brief retorna o valor armazendo no objeto
+         * 
+         * @return string com o valor do tempo no formarto HH:MM
+         */
         string getTime();
+
+        /**
+         * @brief atualiza o valor do objeto em questão
+         * 
+         * @param time novo valor a ser inserido no objeto
+         * @throw invalid_argument caso o valor inserido seja inválido
+         */
         void setTime(string time);
 
 };
