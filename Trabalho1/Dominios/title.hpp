b90a1823891aacf720445a91c6b5d4ee71fcbf01
bomdia12 <chamatico2@gmail.com>
2022-02-25 13:22:30 -0300

    📝 title

--- Trabalho1/Dominios/title.hpp
+++ Trabalho1/Dominios/title.hpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 /**
+ * @brief Classe do Domínio Título
+ * 
  * @author Gabriel Cruz - 200049038
  */
 class Title {
@@ -10,11 +12,34 @@ class Title {
         string title;
         void validate(string title);
     public:
-        Title();
-        Title(string title);
-        string getTitle();
-        void setTitle(string title);
 
+        /**
+         * @brief Construtor padrão do domínio Título
+         */
+        Title();
+
+        /**
+         * @brief Construct a new Title object
+         * 
+         * @param title valor a ser inicializado no objeto
+         * @throw invalid_argument caso algum valor não seja válido
+         */
+        Title(string title);
+
+        /**
+         * @brief retorna o valor armazenado no objeto em questão
+         * 
+         * @return string com o valor do título
+         */
+        string getTitle();
+
+        /**
+         * @brief atualiza o valor o título
+         * 
+         * @param title novo valor do título
+         * @throw invalid_argument caso o valor inserido não seja válido
+         */
+        void setTitle(string title);
 };
 
 inline string Title::getTitle() {
