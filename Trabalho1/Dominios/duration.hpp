e18a94241fc4a411db16dc9f39b6e8514728646f
bomdia12 <chamatico2@gmail.com>
2022-02-25 11:29:57 -0300

    📝 documentation duration

--- Trabalho1/Dominios/duration.hpp
+++ Trabalho1/Dominios/duration.hpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 /**
+ * @brief Classe do domínio duração
+ * 
  * @author Pedro Venzi -  200042696
  */
 class Duration {
@@ -13,9 +15,33 @@ class Duration {
         };
 
     public:
+
+        /**
+         * @brief Construtor padrão do domínio duração
+         */
         Duration();
+
+        /**
+         * @brief Construtor que inicializa o objeto com um valor
+         * 
+         * @param value valor a inicializar o objeto
+         * @throw invalid_arugment caso o valor inserido não seja válido
+         */
         Duration(int value);
+
+        /**
+         * @brief Retorna o valor da 
+         * 
+         * @return int com o valor guardado no objeto em questão
+         */
         int getDuration();
+
+        /**
+         * @brief insere um novo valor no objeto em questão
+         * 
+         * @param value valor a ser inserido como duração
+         * @throw invalid_argument caso o valor inserido não seja válido
+         */
         void setDuration(int value);
 };
 
