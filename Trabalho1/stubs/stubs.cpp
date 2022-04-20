6844030de9859668e83890d0516b87e919e97b84
bomdia12 <chamatico2@gmail.com>
2022-04-19 22:50:21 -0300

    ✨ conta stubs

--- Trabalho1/stubs/stubs.cpp
+++ Trabalho1/stubs/stubs.cpp
@@ -1,7 +1,34 @@
 #include "stubs.h"
 
-bool StubServicoAutenticacao::autenticar(Email email, Password password){
+bool StubServicoAutenticacao::autenticar(Email email, Password password) {
     if(email.getEmail().compare(INVALIDO) == 0 || password.getPassword().compare(INVALIDO) == 0)
         return false;
+    return true;
+}
+
+// Autor: Arthur Mota Furtado - 200014935
+bool StubServicoConta::cadastrarConta(User *user) {
+    if (user->getEmail().compare(EMAIL_INVALIDO) == 0) {
+        return false;
+    }
+
+    return true;
+}
+
+// Autor: Arthur Mota Furtado - 200014935
+bool StubServicoConta::excluirConta(Email *email)  {
+    if (email->getEmail().compare(EMAIL_INVALIDO) == 0) {
+        return false;
+    }
+
+    return true;
+}
+
+// Autor: Arthur Mota Furtado - 200014935
+bool StubServicoConta::editarConta(Email *email, Name name, Password password) {
+    if (email->getEmail().compare(EMAIL_INVALIDO) == 0) {
+        return false;
+    }
+
     return true;
 }
--- Trabalho1/stubs/stubs.h
+++ Trabalho1/stubs/stubs.h
@@ -2,6 +2,7 @@
 #define STUBS
 
 #include "../Interfaces/Servicos/autenticacao.h"
+#include "../Interfaces/Servicos/conta.h"
 #include "../Entidades/user.hpp"
 #include "../Dominios/email.hpp"
 #include "../Dominios/password.hpp"
@@ -13,6 +14,18 @@ class StubServicoAutenticacao:public IServicoAutenticacao {
         bool autenticar(Email, Password);
 };
 
+// Autor: Arthur Mota Furtado - 200014935
+class StubServicoConta: public IServicoConta {
+    private:
+        static const string EMAIL_INVALIDO;
+    public:
+        bool cadastrarConta(User*);
+        bool excluirConta(Email*);
+        bool editarConta(Email*, Name, Password);
+};
+
 inline const string StubServicoAutenticacao::INVALIDO = "0000000000";
+
+inline const string StubServicoConta::EMAIL_INVALIDO = "email@dominio";
 
 #endif
